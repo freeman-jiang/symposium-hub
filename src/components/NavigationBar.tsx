fcb77659ec1dcd1cd2cbbcb69ca6a364f92a16ee
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-03-13 01:04:50 -0400

    icon update

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,3 +1,10 @@
+import {
+  HouseIcon,
+  PresentationIcon,
+  SparklesIcon,
+  UsersIcon,
+  WaypointsIcon,
+} from "lucide-react";
 import Link from "next/link";
 import { SocraticaLogo } from "./icons/SocraticaLogo";
 import NavIcon from "./NavIcon";
@@ -13,90 +20,23 @@ export default function NavigationBar() {
       {/* Navigation icons */}
       <div className="flex gap-2">
         <NavIcon href="#" active={true}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-          </svg>
+          <HouseIcon className="size-5" />
         </NavIcon>
 
         <NavIcon href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="18" cy="5" r="3" />
-            <circle cx="6" cy="12" r="3" />
-            <circle cx="18" cy="19" r="3" />
-            <line x1="8.59" y1="13.51" x2="15.42" y2="17.49" />
-            <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
-          </svg>
+          <UsersIcon className="size-5" />
         </NavIcon>
 
         <NavIcon href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="12" cy="12" r="2" />
-            <path d="M16.24 7.76a6 6 0 0 1 0 8.49m-8.48-.01a6 6 0 0 1 0-8.49m11.31-2.82a10 10 0 0 1 0 14.14m-14.14 0a10 10 0 0 1 0-14.14" />
-          </svg>
+          <SparklesIcon className="size-5" />
         </NavIcon>
 
         <NavIcon href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M12 2 2 7l10 5 10-5-10-5Z" />
-            <path d="m2 17 10 5 10-5" />
-            <path d="m2 12 10 5 10-5" />
-          </svg>
+          <WaypointsIcon className="size-5" />
         </NavIcon>
 
         <NavIcon href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 7.65l.77.77L12 21.35l7.65-8.35.77-.77a5.4 5.4 0 0 0 0-7.65z" />
-          </svg>
+          <PresentationIcon className="size-5" />
         </NavIcon>
       </div>
     </div>
