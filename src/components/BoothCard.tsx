93d6eaf47ffa73e35844ef9730f3d0d46d49d034
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-03-14 19:28:04 -0400

    v1 booths

---
+++ src/components/BoothCard.tsx
@@ -0,0 +1,36 @@
+import { HTMLAttributes } from "react";
+import { cn } from "../lib/utils";
+
+export interface BoothCardProps extends HTMLAttributes<HTMLDivElement> {
+  title: string;
+  description: string;
+  members: string[];
+}
+
+export default function BoothCard({
+  title,
+  description,
+  members,
+  className,
+  ...props
+}: BoothCardProps) {
+  const truncatedDescription =
+    description.length > 200 ? description.slice(0, 200) + "..." : description;
+
+  return (
+    <div className={cn("", className)} {...props}>
+      <div className="bg-black hover:bg-zinc-900 transition-colors text-white max-w-md rounded mt-4 p-8 w-full h-full duration-300">
+        <h3
+          className="text-[1.7rem] font-tiempos mb-2 text-white"
+          style={{
+            lineHeight: "1.15",
+          }}
+        >
+          {title}
+        </h3>
+        <p className="mb-4 text-zinc-100">{members.join(", ")}</p>
+        <p className="text-sm text-zinc-400">{truncatedDescription}</p>
+      </div>
+    </div>
+  );
+}
