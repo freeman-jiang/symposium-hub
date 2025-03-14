fd9057342277bd86129faa42cfa01795d649af4d
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-03-14 02:49:50 -0400

    decrease title size

--- src/components/DemoCard.tsx
+++ src/components/DemoCard.tsx
@@ -20,7 +20,14 @@ export default function DemoCard({
       <BlueGuy className="w-28" />
 
       <div className="bg-black text-white max-w-md rounded-lg mt-4 p-4">
-        <h3 className="text-3xl font-tiempos mb-2 text-white">{title}</h3>
+        <h3
+          className="text-[1.7rem] font-tiempos mb-2 text-white"
+          style={{
+            lineHeight: "1.15",
+          }}
+        >
+          {title}
+        </h3>
         <p className="mb-4 text-zinc-100">{author}</p>
         <p className="text-sm text-zinc-400">{description}</p>
       </div>
