3759358ba9821756c9b99361740f74f1ba833745
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-03-17 16:04:20 -0400

    reorder for demo order

--- src/app/stage/page.tsx
+++ src/app/stage/page.tsx
@@ -17,12 +17,28 @@ import { motion } from "motion/react";
 
 const DEMO_DATA: DemoCardProps[] = [
   {
-    title: "Eyes on the T",
-    author: "Arielle Lok",
+    title: "Arterial",
+    author: "Rishi Kothari",
     description:
-      "The humorous story of putting googly eyes on a train to raise awareness for public transit, and possibly the story of starting 3+ Socratica nodes.",
-    icon: Arielle,
-    href: "https://x.com/ariellelok",
+      "The inspirational story of starting a company that is modernizing urban planning and policy, and bringing more awareness to the importance of this field.",
+    icon: Rishi,
+    href: "https://www.arterial.design/",
+  },
+  {
+    title: "Writing Journey",
+    author: "Georgia Berg",
+    description:
+      "The inspirational story of pursuing writing, and finding what one is truly passionate about.",
+    icon: Georgia,
+    href: "https://hotfemoid.substack.com/",
+  },
+  {
+    title: "HackerFab",
+    author: "Yash, Arjun, and Team",
+    description:
+      "A group of students who are fabricating their own computer chips (an incredibly challenging feat), and the humorous story of starting in an apartment and now a full design team on campus.",
+    icon: HackerFab,
+    href: "https://x.com/waterloofab",
   },
   {
     title: "Bracket Bots",
@@ -31,6 +47,30 @@ const DEMO_DATA: DemoCardProps[] = [
       "BracketBot: An open source robotics kit, the interesting and humorous story behind them.",
     icon: Brian,
     href: "https://x.com/sincethestudy",
+  },
+  {
+    title: "Art Progression",
+    author: "Jasmine Ju",
+    description:
+      "The inspirational journey of pursuing art over the years, taking risks, and making things that are intrinsic to oneself, regardless of technical perfection.",
+    icon: Jasmine,
+    href: "#",
+  },
+  {
+    title: "Alternative Proteins",
+    author: "Rikard Saqe",
+    description:
+      "The humorous yet awe-inspiring story behind conducting research in lab-grown/cell-based meat for the last 2+ years, and a discussion of alternative proteins and their necessity for our future.",
+    icon: Rikard,
+    href: "#",
+  },
+  {
+    title: "Eyes on the T",
+    author: "Arielle Lok",
+    description:
+      "The humorous story of putting googly eyes on a train to raise awareness for public transit, and possibly the story of starting 3+ Socratica nodes.",
+    icon: Arielle,
+    href: "https://x.com/ariellelok",
   },
   {
     title: "Human Powered Exoskeleton",
@@ -47,46 +87,6 @@ const DEMO_DATA: DemoCardProps[] = [
       "A photobook of Waterloo, and the interesting story behind a long gone historian of Waterloo. Will be showing off a lot of interesting photographs.",
     icon: Kevin,
     href: "https://www.kevinistaking.pictures/",
-  },
-  {
-    title: "HackerFab",
-    author: "Yash, Arjun, and Team",
-    description:
-      "A group of students who are fabricating their own computer chips (an incredibly challenging feat), and the humorous story of starting in an apartment and now a full design team on campus.",
-    icon: HackerFab,
-    href: "https://x.com/waterloofab",
-  },
-  {
-    title: "Alternative Proteins",
-    author: "Rikard Saqe",
-    description:
-      "The humorous yet awe-inspiring story behind conducting research in lab-grown/cell-based meat for the last 2+ years, and a discussion of alternative proteins and their necessity for our future.",
-    icon: Rikard,
-    href: "#",
-  },
-  {
-    title: "Art Progression",
-    author: "Jasmine Ju",
-    description:
-      "The inspirational journey of pursuing art over the years, taking risks, and making things that are intrinsic to oneself, regardless of technical perfection.",
-    icon: Jasmine,
-    href: "#",
-  },
-  {
-    title: "Writing Journey",
-    author: "Georgia Berg",
-    description:
-      "The inspirational story of pursuing writing, and finding what one is truly passionate about.",
-    icon: Georgia,
-    href: "https://hotfemoid.substack.com/",
-  },
-  {
-    title: "Arterial",
-    author: "Rishi Kothari",
-    description:
-      "The inspirational story of starting a company that is modernizing urban planning and policy, and bringing more awareness to the importance of this field.",
-    icon: Rishi,
-    href: "https://www.arterial.design/",
   },
   {
     title: "PRISM Collective",
